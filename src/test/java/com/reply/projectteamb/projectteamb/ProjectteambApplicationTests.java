186dfe3a9e4b76b5d52de64e3b801d7551003a18
p.demuro <p.demuro@reply.it>
2022-04-01 18:24:26 +0200

    first commit

---
+++ src/test/java/com/reply/projectteamb/projectteamb/ProjectteambApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reply.projectteamb.projectteamb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectteambApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
